4aff8e7292de5a810493d7e33b1a531e6e9f88bc
jackbolton901 <bltnjck@gmail.com>
2019-08-15 16:14:08 -0500

    afternoon

---
+++ index.js
@@ -0,0 +1,16 @@
+//1
+
+let myName= "Kevin";
+let myId= "2112";
+let myAddress= "Memphis,TN";
+let myTemplateString= `My name is ${myName}.
+My id is ${myId}.
+My address is ${myAddress}`;
+alert(myTemplateString);
+//2
+var cantestant= "1_M_1337!";
+var score= "9001";
+
+let theTemp= `Congratulations ${cantestant}!!!! Your score is ${score}`
+
+ .
